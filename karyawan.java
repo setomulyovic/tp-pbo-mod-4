5e9747a382f9e2d7edcd4584d9e541517bc754b7
setomulyovic <32457267+setomulyovic@users.noreply.github.com>
2017-10-02 20:41:56 +0700

    Create karyawan.java

---
+++ karyawan.java
@@ -0,0 +1,28 @@
+
+public class karyawan {
+
+   private String nama;
+   private double gaji;
+
+    public karyawan(String nama, double gaji) {
+        this.nama = nama;
+        this.gaji = gaji;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getGaji() {
+        return gaji;
+    }
+
+    public void setGaji(double gaji) {
+        this.gaji = gaji;
+    }  
+    
+}
